da9fe6966d0710a2012ea1f187b558236b3ce522
myroslavHub <mhavdio@gmail.com>
2016-05-27 15:18:09 +0300

    Menu updated. Added Canvas Scaler.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,6 +6,11 @@ public class Move : MonoBehaviour
 
 	private void Start()
 	{
-		GetComponent<Rigidbody2D>().velocity = new Vector2(-Speed, 0);
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(-Speed, 0);
+	}
+
+	void Update()
+	{
+		transform.position += Vector3.left * Speed * Time.deltaTime;
 	}
 }
